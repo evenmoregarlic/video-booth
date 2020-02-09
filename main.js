da6d90b9e6ab013294dee3bbe29a3321f28aa730
Dafydd Thomas <daf.p.thomas@gmail.com>
2020-02-09 10:52:55 +0000

    start making recorder class

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 
 function createWindow () {
@@ -8,7 +8,8 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
     }
   })
 
@@ -26,9 +27,7 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+  app.quit()
 })
 
 app.on('activate', function () {
@@ -39,3 +38,16 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+ipcMain.on('record', (event, arg) => {
+  switch(arg) {
+    case 'start':
+      console.log('start recording');
+      break;
+    case 'stop':
+      console.log('stop recording');
+      break;
+    default:
+      console.log('unknown command, ignoring');
+      break;
+    }
+})
--- renderer.js
+++ renderer.js
@@ -5,14 +5,23 @@
 // selectively enable features needed in the rendering
 // process.
 
+const {ipcRenderer } = require('electron');
 
 // Grab elements, create settings, etc.
 var video = document.getElementById('video');
 
+const windowWidth = document.getElementById('video-container').offsetWidth;
+const windowHeight = document.getElementById('video-container').offsetHeight;
+
 // Get access to the camera!
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: {width: {min: 800}, height: {min: 600}} }).then(function(stream) {
+    navigator.mediaDevices.getUserMedia({
+         video: {
+             facingMode: 'user',
+             width: {max: windowWidth}, 
+        } 
+    }).then(function(stream) {
         //video.src = window.URL.createObjectURL(stream);
         video.srcObject = stream;
         video.play();
@@ -20,4 +29,10 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 
 }
 
-console.log("hi?")
+
+document.getElementById('record-start').onclick = function record() {
+    ipcRenderer.send('record', 'start');
+}
+
+document.getElementById('record-stop').onclick = () => ipcRenderer.send('record', 'stop');
+
